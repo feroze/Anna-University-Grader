61d053ac28130e0cd3a21483dd027f3d8f7a76af
Feroze Naina <zoreph@feroze.in>
2011-03-03 21:19:13 +0530

    Name, Roll extracted

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ URL="http://www1.annatech.ac.in/result/index.php?regno=MTAyNzYwNg=="
 http_pool = urllib3.connection_from_url(URL)
 fields = {'regnum':regno,'Submit':'Submit'}
 page=http_pool.post_url(URL,fields)
-print page.data
+#print page.data
+
+soup=BeautifulSoup(page.data)
+results = soup.findAll('span', attrs={'style' : "font-weight:bold;color:#003366;"})
+
+roll=results[0].renderContents()
+name=results[1].renderContents()
+print roll ," - ", name
 
 
 
